1b762b1b8fa17143a2afda9c845926c2aac5b161
Greg Brown <greg@12dnetworks.com>
2021-06-15 16:25:37 -0700

    v1.0.2 use triniti/schemas v1.1.16

---
+++ build/php/src/Extra/Schemas/Iam/Node/TwitterApp.php
@@ -0,0 +1,9 @@
+<?php
+// @link https://schemas.extratv.com/json-schema/extra/iam/node/twitter-app/latest.json#
+namespace Extra\Schemas\Iam\Node;
+
+use Gdbots\Pbj\Message;
+
+interface TwitterApp extends Message
+{
+}
---
+++ build/php/src/Extra/Schemas/Notify/Node/TwitterNotification.php
@@ -0,0 +1,9 @@
+<?php
+// @link https://schemas.extratv.com/json-schema/extra/notify/node/twitter-notification/latest.json#
+namespace Extra\Schemas\Notify\Node;
+
+use Gdbots\Pbj\Message;
+
+interface TwitterNotification extends Message
+{
+}
